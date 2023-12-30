e85961146c2420dcf4d6d0652dfd9564f5b957d1
Ramu Mohan Srinivasan Radhakrishnan <ramumsrk02071981@gmail.com>
2023-12-30 18:47:38 +0530

    On branch master-mabroukmahdhi.medium.com
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   understanding-the-difference-between-string-and-string-in-c/understanding_the_difference_between_String_and_string_in_csharp/Program.cs
    	new file:   understanding-the-difference-between-string-and-string-in-c/understanding_the_difference_between_String_and_string_in_csharp/understanding_the_difference_between_String_and_string_in_csharp.csproj

---
+++ understanding-the-difference-between-string-and-string-in-c/understanding_the_difference_between_String_and_string_in_csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿System.String mediumAuthor = "Mabrouk";
+
+System.Console.WriteLine( mediumAuthor.ToLower() );
+
+string anotherMediumAuthor = "John";
+
+System.Console.WriteLine( anotherMediumAuthor.ToLower() );
+
+System.String firstName = "Mabrouk";
+string lastName = "Mahdhi";
+System.String fullName = firstName + " " + lastName;
+System.Console.WriteLine( $"Full name: '{fullName}'" );
